a54f41282c6c5192697742b810f6435be127ec29
Nick Schmidt <nschmidt@GF-BRIS.local>
2018-07-19 15:50:15 +1000

    Add project files.

---
+++ TryItOut/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TryItOut
+{
+    class Game
+    {
+        public static void StartGame()
+        {
+
+            Console.WriteLine("Welcome to America");
+
+        }
+
+        public static void CharacterName()
+        {
+            Console.WriteLine("Please enter your name");
+            var CharacterName = Console.ReadLine();
+            Console.WriteLine($"\nHi {CharacterName}, America is a land of mystical creatures and hella fatties. \nIt's dangerous. \nIt's unhealthy.\nAll you can do is just...\nTry it out. ");
+        }
+       
+    }
+}
---
+++ TryItOut/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryItOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
